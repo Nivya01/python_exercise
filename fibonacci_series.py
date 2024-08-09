f6aa1b3ab33e4208a731d4b58b097a773cf11e95
nivyaathmanathan@gmail.com <nivyaathmanathan@gmail.com>
2024-08-09 10:07:07 +0530

    modify fibonacci_series file

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,8 +1,8 @@
 n_terms = 10
-fibonacci_sequence = [0, 1]
+fibonacci_series = [0, 1]
 
 for i in range(n_terms):
-    next_term = fibonacci_sequence[-1] + fibonacci_sequence[-2]
-    fibonacci_sequence.append(next_term)
+    next_term = fibonacci_series[-1] + fibonacci_series[-2]
+    fibonacci_series.append(next_term)
 
-print(fibonacci_sequence)
+print(fibonacci_series)

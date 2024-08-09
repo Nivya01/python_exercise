1cced912014b2e4c181315dec4f6ce667ce8bcf9
nivyaathmanathan@gmail.com <nivyaathmanathan@gmail.com>
2024-08-09 10:24:32 +0530

    create and write the code of number guessing game

---
+++ number_guessing.py
@@ -0,0 +1,14 @@
+
+import random
+import sys
+print('\n\n')
+guess = int(input('Guess the number from 1 to 10:'))
+if guess <1 or  guess>10:
+    sys.exit("You entered invalid number")
+computer = random.randint(1,10)
+print("Computer chose number: ",computer)
+if guess == computer:
+    print("yeah hoo,Your guess is correct",'\n')
+else:
+    print("oh no,Your guess is wrong",'\n')
+
